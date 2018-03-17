e6ca56a91d448f8d834ec235aacfea7eaeadb722
Brian Lee <blee025432@gmail.com>
2018-03-17 18:00:39 -0400

    set up jewelry model

--- newtown/jewelry/models.py
+++ newtown/jewelry/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Jewelry(models.Model):
+	jewelry_type = models.CharField(max_length=50)
+	size = models.IntegerField(default=0)
+	metal = models.CharField(max_length=50)
+	price = models.DecimalField(max_digits=6,decimal_places=2)
+	#to add image later
