dcfe12834f5c409772c8e1990745789914004553
Brian Lee <blee025432@gmail.com>
2018-03-17 18:25:15 -0400

    brushed up template

--- newtown/jewelry/views.py
+++ newtown/jewelry/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 def index(request):
-	return HttpResponse("ttesettse")
+	all_jewelry = Jewelry.objects.all()
+	return render(request, 'jewelry/jewelry_list.html',{'all_jewelry':all_jewelry})
+def gold(request):
+	gold_jewelry = Jewelry.objects.filter(metal = 'Gold')
 # Create your views here.
