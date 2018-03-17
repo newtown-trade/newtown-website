6d08d6d3c073f3bf8e4d02625a0cec0ac21896f6
Brian Lee <blee025432@gmail.com>
2018-03-17 18:09:57 -0400

    integrated test admin wtih jewelry

--- newtown/jewelry/models.py
+++ newtown/jewelry/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 class Jewelry(models.Model):
 	jewelry_type = models.CharField(max_length=50)
-	size = models.IntegerField(default=0)
+	size = models.IntegerField(default=0)#change to float later if necessary
 	metal = models.CharField(max_length=50)
 	price = models.DecimalField(max_digits=6,decimal_places=2)
 	#to add image later
 
 	def __str__(self):
-		output = self.metal + ' ' + self.jewelry_type + ', ' + str(self.metal)
+		output = self.metal + ' ' + self.jewelry_type + ', ' + str(self.size) + ' mm.'
+		return output
