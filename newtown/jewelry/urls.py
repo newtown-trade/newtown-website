0879dd83e7c148667018426a22cc9fbf26aba60d
Brian Lee <blee025432@gmail.com>
2018-03-19 21:11:56 -0400

    implemented temporarymetal-specific views

--- newtown/jewelry/urls.py
+++ newtown/jewelry/urls.py
@@ -2,6 +2,12 @@ from django.conf.urls import url, include
 
 from . import views
 
-urlpatterns = [
+urlpatterns = [#need to write generic view for specific metals and for specific materials
 	url(r'^$',views.index,name='index'),
+	url(r'^gold/$',views.gold, name='gold'),
+	url(r'^silver/$',views.silver, name='silver'),
+	url(r'^bronze/$',views.bronze, name='bronze'),
+	url(r'^steel/$',views.steel, name='steel'),
+
+
 ]
--- newtown/jewelry/views.py
+++ newtown/jewelry/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
-
+#need to write generic view for specific metals and for specific materials
 def index(request):
-	all_jewelry = Jewelry.objects.all()
-	return render(request, 'jewelry/jewelry_list.html',{'all_jewelry':all_jewelry})
+	jewelry_items= Jewelry.objects.all()
+	print(len(jewelry_items))
+	return render(request, 'jewelry/jewelry_list.html',{'jewelry_items':jewelry_items})
 def gold(request):
-	gold_jewelry = Jewelry.objects.filter(metal = 'Gold')
-# Create your views here.
+	jewelry_items = Jewelry.objects.filter(metal = 'Gold')
+	return render(request, 'jewelry/jewelry_list.html',{'jewelry_items':jewelry_items})
+def silver(request):
+	jewelry_items = Jewelry.objects.filter(metal = 'Silver')
+	return render(request, 'jewelry/jewelry_list.html',{'jewelry_items':jewelry_items})
+def bronze(request):
+	jewelry_items = Jewelry.objects.filter(metal = 'Bronze')
+	return render(request, 'jewelry/jewelry_list.html',{'jewelry_items':jewelry_items})
+def steel(request):
+	jewelry_items = Jewelry.objects.filter(metal = 'Steel')
+	return render(request, 'jewelry/jewelry_list.html',{'jewelry_items':jewelry_items})
