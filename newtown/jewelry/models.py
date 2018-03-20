4b447cd7b26858c6820b773c8003b2ce0ace9a3f
Brian Lee <blee025432@gmail.com>
2018-03-19 20:49:45 -0400

    implemented duple option in jewelry metals

--- newtown/jewelry/models.py
+++ newtown/jewelry/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 
 class Jewelry(models.Model):
+	GOLD = 'Gold'
+	SILVER = 'Silver'
+	BRONZE = 'Bronze'
+	STEEL = 'Steel'
+	
+	METAL_CHOICES = (
+		(GOLD,'Gold'),
+		(SILVER,'Silver'),
+		(BRONZE,'Bronze'),
+		(STEEL,'Steel'),	
+	)	
 	jewelry_type = models.CharField(max_length=50)
 	size = models.IntegerField(default=0)#change to float later if necessary
-	metal = models.CharField(max_length=50)
+	metal = models.CharField(max_length=50, choices=METAL_CHOICES,default=GOLD)
 	price = models.DecimalField(max_digits=6,decimal_places=2)
 	#to add image later
 
