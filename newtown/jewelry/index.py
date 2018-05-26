9cf946fa9add899acd147e66d7619795bf862402
Brian Lee <blee025432@gmail.com>
2018-05-25 23:39:57 -0400

    added widget for size

--- newtown/jewelry/index.py
+++ newtown/jewelry/index.py
@@ -1,7 +1,8 @@
 #additional settings for Algolia's Indexing of the Jewelry class
 #this is where facets (i.e. search refinement options are declared)
+#attributesForFaceting should ALWAYS be model fields ONLY (this should be automated)
 
 from algoliasearch_django import AlgoliaIndex
 
 class MetalIndex(AlgoliaIndex):
-	settings={'attributesForFaceting':['metal','jewelry_type','price']}
+	settings={'attributesForFaceting':['metal','jewelry_type','price','size']}
