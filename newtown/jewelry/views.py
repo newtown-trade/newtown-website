6a15f4c8b45e731e2c359ca14d2469bf7c0bac52
Brian Lee <blee025432@gmail.com>
2018-06-02 20:33:19 -0400

    basic item-specific system implemented

--- newtown/jewelry/views.py
+++ newtown/jewelry/views.py
@@ -1,16 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from django.db.models import Max, Min
 from .models import *
 #need to write generic view for specific metals and for specific materials
 def index(request):
-	return render(request,'jewelry/jewelry_root.html',{''})
+	return render(request,'jewelry/jewelry_root.html',{})
 def metals(request):
 	metal_choices = Metal.METAL_CHOICES
 	jewelry_items= Metal.objects.all()
 	price_range = [jewelry_items.aggregate(Min('price')),jewelry_items.aggregate(Max('price'))]
 	size_range = [jewelry_items.aggregate(Min('size')),jewelry_items.aggregate(Max('size'))]
 	return render(request, 'jewelry/jewelry_list.html',{'jewelry_items':jewelry_items})
+def metal_specific(request,metal_id):
+	metal_jewelry = get_object_or_404(Metal,pk=metal_id)
+	return render(request,'jewelry/jewelry_specific.html',{metal_jewelry:'metal_jewelry'})
 '''
 def gold(request):
 	jewelry_items = Jewelry.objects.filter(metal = 'Gold')
