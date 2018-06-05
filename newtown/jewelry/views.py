69ecaad92e095a60a9ae024e9493a9535e9dc449
Brian Lee <blee025432@gmail.com>
2018-06-05 19:35:26 -0400

    basic template set up for display

--- newtown/jewelry/views.py
+++ newtown/jewelry/views.py
@@ -15,17 +15,7 @@ def metal_specific(request,metal_id):
 	print('ObjectID: ' + str(metal_id))
 	metal_jewelry = get_object_or_404(Metal,pk=metal_id)
 	return render(request,'jewelry/jewelry_specific.html',{metal_jewelry:'metal_jewelry'})
-'''
-def gold(request):
-	jewelry_items = Jewelry.objects.filter(metal = 'Gold')
-	return render(request, 'jewelry/jewelry_list.html',{'jewelry_items':jewelry_items})
-def silver(request):
-	jewelry_items = Jewelry.objects.filter(metal = 'Silver')
-	return render(request, 'jewelry/jewelry_list.html',{'jewelry_items':jewelry_items})
-def bronze(request):
-	jewelry_items = Jewelry.objects.filter(metal = 'Bronze')
-	return render(request, 'jewelry/jewelry_list.html',{'jewelry_items':jewelry_items})
-def steel(request):
-	jewelry_items = Jewelry.objects.filter(metal = 'Steel')
-	return render(request, 'jewelry/jewelry_list.html',{'jewelry_items':jewelry_items})
-'''
+def display(request):
+	displays = Display.objects.all()
+	return render(request,'jewelry/display_temp.html')#this will be superseded by a generic view later
+
