cf2a1fb2d7551f5daa2abd0de4466c025e2836d2
Brian Lee <blee025432@gmail.com>
2018-06-06 23:07:10 -0400

    wrote methods for Algolia's widgets

--- newtown/jewelry/views.py
+++ newtown/jewelry/views.py
@@ -23,8 +23,9 @@ def metals(request):
 
 	#pre-generates a url to create specific links for Algolia's objects:	
 	metal_specific_url=reverse('jewelry:metal_specific',args=[1337]).replace('1337','{{objectID}}') #1337 is arbitrary
+	model_name="Metal"
 
-	return render(request, 'jewelry/jewelry_list.html',{'jewelry_items':jewelry_items,'metal_url_specific':metal_specific_url}) 
+	return render(request, 'jewelry/jewelry_list.html',{'jewelry_items':jewelry_items,'metal_url_specific':metal_specific_url,'model_name':model_name}) 
 def metal_specific(request,metal_id):
 	metal_jewelry = get_object_or_404(Metal,pk=metal_id)
 	return render(request,'jewelry/jewelry_specific.html',{metal_jewelry:'metal_jewelry'})
