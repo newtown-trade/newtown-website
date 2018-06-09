4b0c51a16f48273646436e229262fe3b4fedfa81
Brian Lee <blee025432@gmail.com>
2018-06-09 11:55:03 -0400

    wrote generalized specific item method, with bugs

--- newtown/jewelry/views.py
+++ newtown/jewelry/views.py
@@ -11,9 +11,7 @@ def generate_context(get_field_result,model_name,specific_method):
 	#parses out and obtains field types
 	attributes = {}
 	for i in get_field_result:
-		if i.get_internal_type() in ['ManyToManyField','AutoField','FileField','ImageField']:
-			pass
-		else:
+		if i.get_internal_type() not in ['ManyToManyField','AutoField','FileField','ImageField']:
 			attributes[i.name]=[i.verbose_name,str(i.get_internal_type())]
 
 	#generates the item-specific urls
@@ -32,8 +30,21 @@ def metals(request):
 	#return render(request, 'jewelry/jewelry_list.html',{'metal_url_specific':metal_specific_url,'model_name':model_name,'attributes':attributes}) 
 
 def metal_specific(request,metal_id):
-	metal_jewelry = get_object_or_404(Metal,pk=metal_id)
-	return render(request,'jewelry/jewelry_specific.html',{metal_jewelry:'metal_jewelry'})
+
+	#gets the Algolia-Indexable attributes of an object and dynamically grabs them to be displayed
+	#note: this section doesn't actually use Algolia
+	specific_item = get_object_or_404(Metal,pk=metal_id)
+	item_attributes = {}
+	for i in specific_item._meta.get_fields():
+		if i.get_internal_type() not in ['ManyToManyField','AutoField','FileField','ImageField']:		
+			item_attributes[str(i.verbose_name)]=str(getattr(specific_item,str(i.name)))
+	print(item_attributes)
+
+	#gets url to return back to full list of objects	
+	url_to_full_list = reverse('jewelry:metals')
+
+	#TODO: fix 'unhashable type: dict bug'
+	return render(request,'jewelry/jewelry_specific.html',{specific_item:'specific_item',url_to_full_list:'url_to_full_list',item_attributes:'item_attributes'})
 
 def contactLense(request):
 	return HttpResponse('test for contact lense')
