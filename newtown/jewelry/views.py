7b66cbcc0f046cdd1049ba10c244af61d9e64e31
Brian Lee <blee025432@gmail.com>
2018-06-09 15:08:21 -0400

    finish generalized specificitem

--- newtown/jewelry/views.py
+++ newtown/jewelry/views.py
@@ -20,31 +20,39 @@ def generate_context(get_field_result,model_name,specific_method):
 	#returns the context to be fed into the view
 	return {'specific_url':specific_url,'model_name':model_name,'attributes':attributes}
 
+#WARNING: code will change if multiple images
+#given a specific item requested via pk and a url back to the full list, dynamically generates a profile for item and returns context
+#url_to_full_list (i.e. the link back to object type's list (e.g. metals) is generated here to abstract it from the template
+def get_specific_item(specific_item, url_to_full_list):
+
+	#grabs the object's fields and values
+	#note: different dictionary structure than generate_context
+	item_attributes = {}
+	image_url=''
+	for i in specific_item._meta.get_fields():
+		#grabs value for majority of indexable fields
+		if i.get_internal_type() not in ['ManyToManyField','AutoField','FileField','ImageField']:		
+			item_attributes[str(i.verbose_name)]=str(getattr(specific_item,str(i.name)))
+		#separately grabs URL to image and pipes it to optional entry in context
+		elif i.get_internal_type() in ['FileField','ImageField']:
+			image_url=getattr(getattr(specific_item,str(i.name)),'url')
+
+	#compiles context to return and optionally stacks image on top
+	attributes={'specific_item':specific_item,'url_to_full_list':url_to_full_list,'item_attributes':item_attributes}
+	if len(image_url)>0:
+		attributes['image_url']=image_url
+	#return context to be fed back to view
+	return attributes
+
 def index(request):
 	return render(request,'jewelry/jewelry_root.html',{})
 
 def metals(request):
-	#legacy code kept in case something breaks
-
 	return render(request,'jewelry/metal.html',generate_context(Metal._meta.get_fields(),Metal.__name__,'metal_specific'))
 	#return render(request, 'jewelry/jewelry_list.html',{'metal_url_specific':metal_specific_url,'model_name':model_name,'attributes':attributes}) 
 
 def metal_specific(request,metal_id):
-
-	#gets the Algolia-Indexable attributes of an object and dynamically grabs them to be displayed
-	#note: this section doesn't actually use Algolia
-	specific_item = get_object_or_404(Metal,pk=metal_id)
-	item_attributes = {}
-	for i in specific_item._meta.get_fields():
-		if i.get_internal_type() not in ['ManyToManyField','AutoField','FileField','ImageField']:		
-			item_attributes[str(i.verbose_name)]=str(getattr(specific_item,str(i.name)))
-	print(item_attributes)
-
-	#gets url to return back to full list of objects	
-	url_to_full_list = reverse('jewelry:metals')
-
-	#TODO: fix 'unhashable type: dict bug'
-	return render(request,'jewelry/jewelry_specific.html',{specific_item:'specific_item',url_to_full_list:'url_to_full_list',item_attributes:'item_attributes'})
+	return render(request,'jewelry/jewelry_specific.html',get_specific_item(get_object_or_404(Metal,pk=metal_id),reverse('jewelry:metals'))) #see comments for get_specific_item
 
 def contactLense(request):
 	return HttpResponse('test for contact lense')
