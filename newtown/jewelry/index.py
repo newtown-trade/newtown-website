069a17226d320ba5310e348f4aa7fe4959b7f366
Brian Lee <blee025432@gmail.com>
2018-08-21 12:05:42 -0400

    upgrade to 2.1 django

--- newtown/jewelry/index.py
+++ newtown/jewelry/index.py
@@ -4,6 +4,20 @@
 
 from algoliasearch_django import AlgoliaIndex
 
+'''
+from class_parser import *
+
+def get_parameters(classname):
+	obj = class_parser(classname)
+	parameters = []
+	for param in obj._meta.get_fields():
+		if param.get_internal_type() not in ['ManyToManyField','AutoField','FileField','ImageField','DateTimeField']:
+			parameters.append(param)
+	return param
+
+print(get_parameters('Metal'))
+'''
+
 class MetalIndex(AlgoliaIndex):
 	parameters = ['jewelry_type','price','size','jewelry_style']
 	settings={'attributesForFaceting':parameters,'searchableAttributes':parameters,'attributesToHighlight':parameters}
